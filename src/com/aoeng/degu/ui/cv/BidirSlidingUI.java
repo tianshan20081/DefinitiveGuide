7be678a91ceaf06e015c95136dda64b9b5e3e333
tianshan20081 <zhangshch2008@gmail.com>
2013-08-31 20:53:48 +0800

    add BidirSlidingUI.java 双向滑动特效

---
+++ src/com/aoeng/degu/ui/cv/BidirSlidingUI.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.aoeng.degu.ui.cv;
+
+import android.app.Activity;
+
+/**
+ * @author [Aoeng Zhang] @datatime Aug 31, 2013:8:52:08 PM
+ * @Email [zhangshch2008@gmail.com] Aug 31, 2013
+ */
+public class BidirSlidingUI extends Activity {
+	
+
+}
