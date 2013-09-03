4e19fadd5f62ab4dd5f2304dd915ce02f9a612d5
tianshan20081 <zhangshch2008@gmail.com>
2013-09-03 14:38:34 +0800

    add Customer View Contacts

--- src/com/aoeng/degu/ui/CustomerViewUI.java
+++ src/com/aoeng/degu/ui/CustomerViewUI.java
@@ -5,12 +5,18 @@ package com.aoeng.degu.ui;
 
 import com.aoeng.degu.R;
 import com.aoeng.degu.ui.cv.BidirSlidingUI;
+import com.aoeng.degu.ui.cv.Contacts2UI;
+import com.aoeng.degu.ui.cv.ContactsUI;
+import com.aoeng.degu.ui.cv.PhotoWallUI;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
 
 /**
  * @author [Aoeng Zhang] @datatime Sep 2, 2013:4:24:12 PM
@@ -29,6 +35,10 @@ public class CustomerViewUI extends Activity implements OnClickListener {
 		setContentView(R.layout.ui_cv_home);
 
 		this.findViewById(R.id.btnBiDirSlid).setOnClickListener(this);
+		this.findViewById(R.id.btnPhotoWall).setOnClickListener(this);
+		this.findViewById(R.id.btnContacts1).setOnClickListener(this);
+		this.findViewById(R.id.btnContacts2).setOnClickListener(this);
+
 	}
 
 	/*
@@ -45,8 +55,17 @@ public class CustomerViewUI extends Activity implements OnClickListener {
 			intent = new Intent(CustomerViewUI.this, BidirSlidingUI.class);
 			startActivity(intent);
 			break;
-
-		default:
+		case R.id.btnPhotoWall:
+			intent = new Intent(CustomerViewUI.this, PhotoWallUI.class);
+			startActivity(intent);
+			break;
+		case R.id.btnContacts1:
+			intent = new Intent(CustomerViewUI.this, ContactsUI.class);
+			startActivity(intent);
+			break;
+		case R.id.btnContacts2:
+			intent = new Intent(CustomerViewUI.this, Contacts2UI.class);
+			startActivity(intent);
 			break;
 		}
 	}
