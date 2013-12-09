ac2d03c2e3f9f98ff5eb2fd3bf02d60718a671e4
tianshan20081 <zhangshch2008@gmail.com>
2013-12-09 11:51:11 +0800

    Update SimpleWebViewUI.java
    webView 访问 https

--- src/com/aoeng/degu/ui/wv/SimpleWebViewUI.java
+++ src/com/aoeng/degu/ui/wv/SimpleWebViewUI.java
@@ -20,9 +20,31 @@ public class SimpleWebViewUI extends Activity {
 		String url = "http://www.beijing.com.cn/";
 		wvSimple = (WebView) this.findViewById(R.id.wvSimple);
 		
+		
+		//添加对  JavaScript 的支持
+		wvSimple.setJavaScriptEnabled(true);
+		//添加 放大缩小控件
+		wvSimple.setBuiltInZoomControls(true);
+		wvSimple.setSupportZoom(true);
+		wvSimple.setDefaultZoom(ZoomDensity.CLOSE);
+		//初始时候页面总宽度为 手机宽度。
+		wvSimple.setUseWideViewPort(true);
+		wvSimple.setLoadWithOverviewMode(true);
+		
 		if(URLUtil.isHttpsUrl(url)){
 			//请求链接 为 https 访问时
-			
+			wvSimple.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+				handler.proceed();
+			}
+
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				//当页面加载完毕的时候 加载进度条消失
+				ViewUtils.dismissDia(dialog);
+			}
+		});
 		}else if(URLUtil.isHttpUrl(url)){
 			//请求访问为HTTP 访问
 			
