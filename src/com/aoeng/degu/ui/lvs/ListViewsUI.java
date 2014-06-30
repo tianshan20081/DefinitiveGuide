57100c0a8f81f4462a74845bb25fa221f9244dbe
tianshan20081 <zhangshch2008@gmail.com>
2014-06-30 16:05:09 +0800

    增加 listview 滑动删除条目效果,使用scrollview 实现. 不足:直接删除.

--- src/com/aoeng/degu/ui/lvs/ListViewsUI.java
+++ src/com/aoeng/degu/ui/lvs/ListViewsUI.java
@@ -30,6 +30,7 @@ public class ListViewsUI extends Activity implements OnClickListener {
 		this.findViewById(R.id.btnMovingDel).setOnClickListener(this);
 		this.findViewById(R.id.btnUpDownLv).setOnClickListener(this);
 		this.findViewById(R.id.btnLvMargin).setOnClickListener(this);
+		this.findViewById(R.id.btnScrollDel).setOnClickListener(this);
 
 	}
 
@@ -37,6 +38,9 @@ public class ListViewsUI extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
+		case R.id.btnScrollDel:
+			startActivity(new Intent(ListViewsUI.this,LvScrollDelUI.class));
+			break;
 		case R.id.btnLvMargin:
 			startActivity(new Intent(ListViewsUI.this,LvScrollMarginUI.class));
 			break;
