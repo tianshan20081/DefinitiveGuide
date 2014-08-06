7099197539dc1c09372d77e41f6d7428c76af711
tianshan20081 <zhangshch2008@gmail.com>
2014-08-06 14:08:53 +0800

    handler

---
+++ src/com/aoeng/degu/ui/handler/ThreadHandlerUI.java
@@ -0,0 +1,89 @@
+package com.aoeng.degu.ui.handler;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.view.View;
+
+import com.aoeng.degu.R;
+import com.aoeng.degu.ui.BaseUI;
+import com.aoeng.degu.utils.Logger;
+
+public class ThreadHandlerUI extends BaseUI {
+	private static final int SEND_HELLO = 2;
+	private Handler mHandler;
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.btnHandlerHeelo:
+			toast("HELLO");
+			mHandler.obtainMessage(SEND_HELLO, "hello").sendToTarget();
+
+			Logger.i(TAG, "HELLO" + Thread.currentThread().getName());
+			Logger.i(TAG, "HELLO" + Thread.currentThread().getId());
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	@Override
+	protected void loadViewLayout() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void findViewById() {
+		// TODO Auto-generated method stub
+		setContentView(R.layout.ui_handler_thread);
+
+		new CustomerThread().start();
+	}
+
+	@Override
+	protected void setListener() {
+		// TODO Auto-generated method stub
+		findView(R.id.btnHandlerHeelo).setOnClickListener(this);
+	}
+
+	@Override
+	protected void processLogic() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public class CustomerThread extends Thread {
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			Looper.prepare();
+			mHandler = new Handler() {
+				@Override
+				public void handleMessage(Message msg) {
+					// TODO Auto-generated method stub
+					switch (msg.what) {
+					case SEND_HELLO:
+						toast((String) msg.obj
+								+ Thread.currentThread().getName());
+						Logger.i(TAG, (String) msg.obj
+								+ Thread.currentThread().getName());
+						Logger.i(TAG, (String) msg.obj
+								+ Thread.currentThread().getId());
+						break;
+
+					default:
+						break;
+					}
+					super.handleMessage(msg);
+				}
+			};
+
+		}
+
+	}
+}
