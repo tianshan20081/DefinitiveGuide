58439c0c3df9474d4cc7a30ccd3a35067d059bfa
tianshan20081 <zhangshch2008@gmail.com>
2014-08-07 17:40:21 +0800

    COMMIT

--- obj/local/armeabi/objs-debug/defgui/defgui.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/armeabi/objs-debug/defgui/defgui.o: jni/defgui.c \
- /usr/local/dev/android/ndk-r9/platforms/android-9/arch-arm/usr/include/jni.h
-
-/usr/local/dev/android/ndk-r9/platforms/android-9/arch-arm/usr/include/jni.h:
--- obj/local/armeabi/objs-debug/defgui/jiecheng.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/armeabi/objs-debug/defgui/jiecheng.o: jni/jiecheng.cpp \
- /usr/local/dev/android/ndk-r9/platforms/android-9/arch-arm/usr/include/jni.h
-
-/usr/local/dev/android/ndk-r9/platforms/android-9/arch-arm/usr/include/jni.h:
