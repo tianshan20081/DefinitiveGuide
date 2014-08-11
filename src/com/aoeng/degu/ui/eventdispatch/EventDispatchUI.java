ea3a68dedb39d3e74a2ab2022cc1805980ca695d
tianshan20081 <zhangshch2008@gmail.com>
2014-08-11 10:08:21 +0800

    commit

---
+++ src/com/aoeng/degu/ui/eventdispatch/EventDispatchUI.java
@@ -0,0 +1,45 @@
+package com.aoeng.degu.ui.eventdispatch;
+
+import android.view.View;
+
+import com.aoeng.degu.R;
+import com.aoeng.degu.ui.BaseUI;
+/**
+ * 事件分发
+ * @author Shaocheng Zhang 
+ * Aug 11, 2014  9:42:54 AM 2014
+ */
+public class EventDispatchUI extends BaseUI {
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void loadViewLayout() {
+		// TODO Auto-generated method stub
+		setContentView(R.layout.ui_eventdispatch_home);
+
+	}
+
+	@Override
+	protected void findViewById() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void setListener() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void processLogic() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/aoeng/degu/utils/UIUtils.java
@@ -0,0 +1,15 @@
+package com.aoeng.degu.utils;
+
+import com.aoeng.degu.application.DGApplication;
+
+import android.content.Context;
+
+public class UIUtils {
+
+	public static Context getContext() {
+		// TODO Auto-generated method stub
+		
+		return DGApplication.getContext();
+	}
+
+}
