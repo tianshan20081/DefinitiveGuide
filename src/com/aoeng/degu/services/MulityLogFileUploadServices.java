2681a039a831983ec2ecd2f16393b7811a52ce27
tianshan20081 <zhangshch2008@gmail.com>
2014-09-07 02:52:00 +0800

    commit

--- src/com/aoeng/degu/services/MulityLogFileUploadServices.java
+++ src/com/aoeng/degu/services/MulityLogFileUploadServices.java
@@ -1,10 +1,11 @@
 package com.aoeng.degu.services;
 
 import java.io.File;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
+
+import org.apache.http.Header;
 
 import android.app.Service;
 import android.content.Intent;
@@ -17,11 +18,15 @@ import com.aoeng.degu.utils.DataUtils;
 import com.aoeng.degu.utils.FileUploadVO;
 import com.aoeng.degu.utils.common.FileUtils;
 import com.aoeng.degu.utils.common.Logger;
+import com.aoeng.degu.utils.common.UIUtils;
 import com.aoeng.degu.utils.net.URLUtils;
+import com.aoeng.degu.utils.net.asyncthhpclient.AsyncHttpClient;
+import com.aoeng.degu.utils.net.asyncthhpclient.AsyncHttpResponseHandler;
+import com.aoeng.degu.utils.net.asyncthhpclient.RequestParams;
 
-public class LogFileUploadServices extends Service {
+public class MulityLogFileUploadServices extends Service {
 
-	private static final String TAG = LogFileUploadServices.class.getName();
+	private static final String TAG = MulityLogFileUploadServices.class.getName();
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -34,33 +39,64 @@ public class LogFileUploadServices extends Service {
 	public void onCreate() {
 		// TODO Auto-generated method stub
 		super.onCreate();
-		String path = FileUtils.getAppCrashPath();
-		Logger.i(TAG, path);
+		// String path = FileUtils.getAppCrashPath();
+		// Logger.i(TAG, path);
 	}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		// TODO Auto-generated method stub
 
-		FileUploadVO vo = new FileUploadVO();
-		vo.requestUrl = URLUtils.URL_HOST + URLUtils.urlLogsUpload;
-		vo.jsonParser = new LogFileUploadParser();
-		vo.requestDataMap = getNormalFieldMap();
-		vo.fileFieldMap = getFileFieldMap();
-
-		DataUtils.getDateFromServer(vo, new DataCallback<LogFileUploadResult>() {
+		AsyncHttpClient client = new AsyncHttpClient();
+		client.setTimeout(5000);
+		RequestParams params = new RequestParams();
+		params.put("logFile", getFileList());
+//		params.put("name", "hello");
+		String url = URLUtils.URL_HOST + URLUtils.urlLogsUpload;
+		Logger.i(TAG, url);
+		client.post(url, params, new AsyncHttpResponseHandler() {
 
 			@Override
-			public void processData(LogFileUploadResult result, boolean paramBoolean) {
+			public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
 				// TODO Auto-generated method stub
-				if (null != result) {
-					Logger.i(TAG, result.toString());
-					List<String> uploadedFile = result.getFileNames();
-				}
+				UIUtils.toastShow("upload Success");
+				Logger.i(TAG, "onSuccess");
+			}
+
+			@Override
+			public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+				// TODO Auto-generated method stub
+				UIUtils.toastShow("upload Fail");
+				Logger.i(TAG, "onFailure");
 			}
 		});
 
 		return START_STICKY;
+	}
+
+	private ArrayList getFileList() {
+		// TODO Auto-generated method stub
+		ArrayList<File> files = new ArrayList<File>();
+
+		String path = FileUtils.getAppCrashPath();
+		if (!TextUtils.isEmpty(path)) {
+			File fileCrash = new File(path);
+			if (fileCrash.exists() && fileCrash.isDirectory() && fileCrash.list().length > 0) {
+				String[] loges = fileCrash.list();
+				if (null == loges || loges.length == 1) {
+					return null;
+				}
+				File logFile = null;
+				for (String fileName : loges) {
+					logFile = new File(path, fileName);
+					if (logFile.exists() && logFile.isFile()) {
+						files.add(logFile);
+					}
+				}
+			}
+		}
+
+		return files;
 	}
 
 	private HashMap<String, String> getFileFieldMap() {
