b0c0bcf52517eff6de36f48e496c7e9b293a370b
tianshan20081 <zhangshch2008@gmail.com>
2014-12-06 16:51:56 +0800

    update

---
+++ src/com/aoeng/degu/constant/Constants.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.aoeng.degu.constant;
+
+/**
+ * @author sczhang 2014年12月6日 下午3:56:31
+ * @Email {zhangshch0131@126.com}
+ */
+public class Constants {
+
+	public static final long GroupMaxMillisecond = 1000 * 60 * 60;
+
+}
