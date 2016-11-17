f74cc3e31d7c2170f8695231a14f0fbde5e1ef31
mozi <mozi@helijia.com>
2016-11-17 11:35:28 +0800

    u

--- src/com/aoeng/degu/ui/shell/ShellHomeUI.java
+++ src/com/aoeng/degu/ui/shell/ShellHomeUI.java
@@ -1,12 +1,5 @@
 package com.aoeng.degu.ui.shell;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import org.omg.SendingContext.RunTime;
-
 import android.view.View;
 
 import com.aoeng.degu.R;
@@ -15,66 +8,69 @@ import com.aoeng.degu.utils.common.IOUtils;
 import com.aoeng.degu.utils.common.LogUtils;
 import com.aoeng.degu.utils.common.StringUtils;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class ShellHomeUI extends BaseUI {
 
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		switch (v.getId()) {
-		case R.id.btnWifiInfo:
-			String shell = "cat /data/misc/wifi/wpa_supplicant.conf > /mnt/sdcard/df/wifi.log";
-			String result = execute(shell);
-			if (!StringUtils.isEmpty(result)) {
-				LogUtils.e("----->" + result);
-			}
-			break;
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        switch (v.getId()) {
+            case R.id.btnWifiInfo:
+                String shell = "cat /data/misc/wifi/wpa_supplicant.conf > /mnt/sdcard/df/wifi.log";
+                String result = execute(shell);
+                if (!StringUtils.isEmpty(result)) {
+                    LogUtils.e("----->" + result);
+                }
+                break;
 
-		default:
-			break;
-		}
-	}
+            default:
+                break;
+        }
+    }
 
-	private String execute(String shell) {
-		// TODO Auto-generated method stub
-		Runtime runTime = Runtime.getRuntime();
-		try {
-			Process process = runTime.exec(shell);
-			InputStream is = process.getInputStream();
-			InputStream errIs = process.getErrorStream();
-			String errMsg = IOUtils.getStrFromInputStream(errIs);
-			LogUtils.e("error msg" + errMsg);
-			String msg = IOUtils.getStrFromInputStream(is);
-			return msg;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
+    private String execute(String shell) {
+        // TODO Auto-generated method stub
+        Runtime runTime = Runtime.getRuntime();
+        try {
+            Process process = runTime.exec(shell);
+            InputStream is = process.getInputStream();
+            InputStream errIs = process.getErrorStream();
+            String errMsg = IOUtils.getStrFromInputStream(errIs);
+            LogUtils.e("error msg" + errMsg);
+            String msg = IOUtils.getStrFromInputStream(is);
+            return msg;
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-	@Override
-	protected void loadViewLayout() {
-		// TODO Auto-generated method stub
-		setContentView(R.layout.ui_shell_home);
+    @Override
+    protected void loadViewLayout() {
+        // TODO Auto-generated method stub
+        setContentView(R.layout.ui_shell_home);
 
-	}
+    }
 
-	@Override
-	protected void findViewById() {
-		// TODO Auto-generated method stub
+    @Override
+    protected void findViewById() {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	protected void setListener() {
-		// TODO Auto-generated method stub
-		findView(R.id.btnWifiInfo).setOnClickListener(this);
-	}
+    @Override
+    protected void setListener() {
+        // TODO Auto-generated method stub
+        findView(R.id.btnWifiInfo).setOnClickListener(this);
+    }
 
-	@Override
-	protected void processLogic() {
-		// TODO Auto-generated method stub
+    @Override
+    protected void processLogic() {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
 }
